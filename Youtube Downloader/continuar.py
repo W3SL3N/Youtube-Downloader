3c33ed418ccd42689d4e86d7099e3248fbbccb09
W3SL3N <108354947+W3SL3N@users.noreply.github.com>
2022-07-20 10:20:07 -0300

    Cor
    Cor de indicadores alterada para amarelo.

--- Youtube Downloader/continuar.py
+++ Youtube Downloader/continuar.py
@@ -3,12 +3,15 @@ import os
 class Continuar:
 
     def continuar(self, classe):
+        amarelo = '\033[1;33m'
+        reset = '\033[0;0m'
+        
         condicao = 0
         while condicao == 0:
-            sim = ['SIM', 'S', 'YES', 'Y']
-            nao = ['NAO', 'NÃO', 'NO', 'N']
+            sim = ['sim', 's']
+            nao = ['não', 'nao', 'n']
 
-            continuar = input('\n\nContinuar baixando?(S/n): ').upper().strip()
+            continuar = input('\n\nContinuar baixando?' + (amarelo + '(S/n)' + reset) + ': ').lower().strip()
 
             if continuar in sim:
                 a = classe()
@@ -18,13 +21,15 @@ class Continuar:
                 os.system(command=exit())
 
     def tentar_novamente(self, classe):
+        amarelo = '\033[1;33m'
+        reset = '\033[0;0m'        
+        
         condicao = 0
         while condicao == 0:
-            sim = ['SIM', 'S', 'YES', 'Y']
-            nao = ['NAO', 'NÃO', 'NO', 'N']
+            sim = ['sim', 's']
+            nao = ['não', 'nao', 'n']
 
-            continuar = input('\n\nTentar novamente?(S/n): ').upper().strip()
-            print(' ')
+            continuar = input('\n\nTentar novamente?' + (amarelo + '(S/n)' + reset) + ': ').lower().strip()
 
             if continuar in sim:
                 a = classe()
@@ -32,3 +37,6 @@ class Continuar:
 
             if continuar in nao:
                 os.system(command=exit())
+               
+            else:
+                continue
