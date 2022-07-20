f478d085f2104418036a387390bf2f8bb2631ee5
W3SL3N <108354947+W3SL3N@users.noreply.github.com>
2022-07-20 09:58:27 -0300

    Cor, infos
    Cor da barra de progresso alterada para verde.
    Tamanho do arquivo e quanto foi baixado agora aparecem em MB a esquerda da barra de progresso.

--- Youtube Downloader/progresso.py
+++ Youtube Downloader/progresso.py
@@ -13,16 +13,20 @@ class Progresso:
         self.barra_de_progresso(filesize=filesize, bytes_restantes=bytes_restantes, baixado=baixado)
 
     def barra_de_progresso(self, filesize, bytes_restantes, baixado):
-
-        porcentagem = f'{baixado/filesize:0.2%}'
+        verde = '\033[0;32m'
+        reset = '\033[0;0m'
+        
+        mbs_baixados = f'{int(baixado)}/{int(filesize)}'
 
         # barra de progresso ----------------------------------------------------
         progresso = (filesize - bytes_restantes) / (filesize)
         colunas = int(60 * progresso)
-        status = '█' * colunas + '▒' * (60 - colunas)
+        status = (verde + '█' + reset) * colunas + '▒' * (43 - colunas)
         # -----------------------------------------------------------------------
+        
+        porcentagem = f'{baixado/filesize:0.2%}'
 
-        barra = f'|{status}|{porcentagem:>8}\r'
+        barra = f'[ {mbs_baixados:^10}MB ] |{status}| {porcentagem}\r'
 
         sys.stdout.write(barra)
         sys.stdout.flush()
