a97d905ec6f140c805b98527d5a97803747431cc
W3SL3N <108354947+W3SL3N@users.noreply.github.com>
2022-07-20 10:21:57 -0300

    Cor
    Cor do titulo alterada para amarelo

--- Youtube Downloader/cabecalho.py
+++ Youtube Downloader/cabecalho.py
@@ -4,8 +4,12 @@ class Cabecalho:
 
     def cabecalho(self, titulo):
         os.system('cls')
+
+        amarelo = '\033[1;33m'
+        reset = '\033[0;0m'
+        titulos = amarelo + f'{titulo}' + reset
         print('=' * 70)
         print('\n' + 'Youtube Donwloader'.center(70, ' '))
         print('\n' + '=' * 70)
-        print(f'[{titulo}]'.center(70, ' '))
+        print(f'[{titulos}]'.center(82, ' '))
         print('=' * 70 + '\n')
