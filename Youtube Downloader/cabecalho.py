7f53734150c8a018cacb719cd6f6a65634c58749
W3SL3N <108354947+W3SL3N@users.noreply.github.com>
2022-07-21 17:07:21 -0300

    Atualização
    Funcionalidade de limpeza do terminal adaptada para linux e macos

--- Youtube Downloader/cabecalho.py
+++ Youtube Downloader/cabecalho.py
@@ -1,13 +1,21 @@
-import os
+import os, platform
 
 class Cabecalho:
 
     def cabecalho(self, titulo):
-        os.system('cls')
+        op = platform.system().lower()
+
+        if op == 'windows':
+            os.system('cls')
+
+        else:
+            os.system('clear')
 
         amarelo = '\033[1;33m'
         reset = '\033[0;0m'
+
         titulos = amarelo + f'{titulo}' + reset
+
         print('=' * 70)
         print('\n' + 'Youtube Donwloader'.center(70, ' '))
         print('\n' + '=' * 70)
